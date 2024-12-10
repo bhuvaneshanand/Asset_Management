122f57aa6734609ff5a3be5468460c4d31db309c
bhuvaneshanand <bhuvaneshanand1970@gmail.com>
2024-12-10 19:56:37 +0530

    delete files

--- AssetManagement/src/context/useAuth.jsx
+++
@@ -1,9 +0,0 @@
-import { createContext, useContext } from "react";
- 
-import React from 'react'
-import { AuthContext } from "./AuthProvider";
- 
-export const useAuth = () => {
-  return useContext(AuthContext)
- 
-}
